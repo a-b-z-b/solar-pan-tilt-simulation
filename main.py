d0381c44cce74d4da6a36c4f6a3ef7c337826cc9
zZ7_aki <zakariae.bouklah18@gmail.com>
2025-05-29 17:42:57 +0100

    Accessing Arduino board via serial port: sending solar data & receiving feedback from arduino.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import time
 import schedule
+import serial
 
 from qweather import solar_angles
 from datetime import datetime, time as dt_time
@@ -9,12 +10,20 @@ from dotenv import load_dotenv
 COUNT = 0
 
 
-def scheduled_request(url):
+def scheduled_request(url, arduino):
     global COUNT
 
     solar_data = solar_angles.make_request(url)
     print(f"\nResponse at {datetime.now()}\n")
     print(f"\nelevation: {solar_data['solarElevationAngle']}, azimuth: {solar_data['solarAzimuthAngle']}\n")
+
+    message = f"{solar_data['solarAzimuthAngle']},{solar_data['solarElevationAngle']}\n"
+    arduino.write(message.encode('utf-8'))
+
+    time.sleep(0.1)
+    while arduino.in_waiting:
+        print(f"\nARDUINO SAID: {arduino.readline().decode('utf-8').strip()}\n")
+
     COUNT += 1
     print(f"COUNT: {COUNT}")
     if COUNT >= len(schedule.jobs):
@@ -36,6 +45,9 @@ def main():
 
     now = datetime.now().time()
 
+    arduino = serial.Serial(port='COM7', baudrate=9600, timeout=1)
+    time.sleep(2)
+
     for t in day_distribution:
         url = (
             f"https://devapi.qweather.com/v7/astronomy/solar-elevation-angle?"
@@ -46,7 +58,8 @@ def main():
 
         if sched_time >= now:
             print(f"Scheduling call at {t[:2]}:{t[2:]}")
-            schedule.every().day.at(f"{t[:2]}:{t[2:]}").do(lambda url=url: scheduled_request(url))
+            schedule.every().day.at(f"{t[:2]}:{t[2:]}").do(lambda url=url, arduino=arduino: scheduled_request(url, arduino))
+
         else:
             print(f"Skipping past time {t[:2]}:{t[2:]}")
 
